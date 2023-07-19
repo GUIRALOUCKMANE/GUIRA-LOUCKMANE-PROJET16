184c497ef151a6b83d2d9e0eb2bc27af2053ce14
GUIRA LOUCKMANE <guiralouckmane508@gmail.com>
2023-07-19 17:45:11 +0000

    permier commit

---
+++ index.js
@@ -0,0 +1,99 @@
+var unTexte="voici un texte";
+console.log(unTexte);
+unTexte="nouveau texte...";
+console.log(unTexte);
+const Prenom="Justine";
+console.log(Prenom);
+let unChiffre=24;
+console.log("unChiffre");
+let Chaine="Je suis l autre chaine de caractere";
+let NouvelleChaine="Le chiffre attendu est:s{chiffre} + chiffre + degre";
+console.log(NouvelleChaine);
+let chaineDeCaractere="je suis une chaine ";
+let chiffre=24;
+let boolean=true;
+let array =["je","suis",24,false]
+let object = {
+    Prenom:"audrey",
+    age:34,
+    ville:"Bobo",
+};
+let arbre;
+console.log(arbre);
+console.log(4+5);
+let total= 2;
+total++;
+total++;
+total++;
+total++;
+total++;
+total=5;
+let x =2;
+let y =5;
+if (x < y){
+    console.log("x est inferieur a y");
+} else{
+    console.log("x est superieur a y");
+    
+}
+if (x == y) {
+    console.log("true!");
+    }
+else  {(x == y)} 
+function faireQuelqueChose(){
+    console.log("je fais un true!");
+    console.log("Trop Cool");
+}
+faireQuelqueChose();
+const faireUneTache =(tache)=>{
+    console.log("je fais:" + tache);
+    
+};
+faireUneTache("les courses");
+faireUneTache("le menage")
+function calc(x,y) {
+ return x + y;  
+  }
+    calc(4,9);
+    (function maFonction(){
+        console.log("je ne joue toute seule");
+    })();
+     {
+        console.log("je me joue toute seule ")
+    }
+     function addition(x,y){
+         total= x + y;
+         return x + y;
+     }
+     function soustraction(x){
+         total-= x;
+         return total;
+     }
+     function division(x){
+         total/= x;
+         return total;
+     } 
+         total
+     
+         
+     function multiplication(x){
+         if (total === 0) {
+             return (
+                 total = x
+             );
+         }
+     }
+    
+        
+    
+
+
+
+
+
+
+
+
+
+
+
